3a7e9eeb0edaf7ee9713cad3b3e9f82c85dff984
marianahiath <maarianahiath@gmail.com>
2021-06-04 23:20:49 -0300

    clear fields on submit

--- src/components/formulario/formulario.jsx
+++ src/components/formulario/formulario.jsx
@@ -5,24 +5,27 @@ export default class Formulario extends Component {
 
     constructor(props) {
         super(props);
-        this.titulo = "";
-        this.texto = "";
+        this.state = {
+            titulo: "",
+            texto: ""
+        }
     }
 
     _handleMudancaTitulo(evento) {
         evento.stopPropagation();
-        this.titulo = evento.target.value;
+        this.setState({titulo: evento.target.value, texto: this.state.texto})
     }
 
     _handleMudancaTexto(evento) {
         evento.stopPropagation();
-        this.texto = evento.target.value;
+        this.setState({titulo: this.state.titulo, texto: evento.target.value})
     }
 
     _criarNota(evento) {
         evento.preventDefault();
         evento.stopPropagation();
-        this.props.criarNota(this.titulo, this.texto);
+        this.props.criarNota(this.state.titulo, this.state.texto);
+        this.setState({titulo: "", texto: ""});
     }
 
     render() {
@@ -33,6 +36,7 @@ export default class Formulario extends Component {
                     type="text"
                     placeholder="Título"
                     className="form-cadastro_input"
+                    value={this.state.titulo}
                     onChange={this._handleMudancaTitulo.bind(this)}
 
                 />
@@ -40,6 +44,7 @@ export default class Formulario extends Component {
                     rows={15}
                     placeholder="Escreva sua nota..."
                     className="form-cadastro_input"
+                    value={this.state.texto}
                     onChange={this._handleMudancaTexto.bind(this)}
                 />
                 <button className="form-cadastro_input form-cadastro_submit">
